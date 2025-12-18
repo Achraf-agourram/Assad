a6b4f5de894ae49096dcfd5387d7190abf58256e
Achraf-podcast <achrafagourram6@gmail.com>
2025-12-18 15:15:09 +0100

    setting the function get_data that convert any sql request to result of array containing table of rows, and extract_rows

--- database.php
+++ database.php
@@ -1,8 +1,26 @@
 <?php
 $database = new mysqli("localhost", "root", "", "zoo_assad");
 
-function get_data($command){
-    echo $command;
+function extract_rows($table){
+    $rowsArray = [];
+    while($row = $table->fetch_assoc()){
+        array_push($rowsArray, $row);
+    }
+    return $rowsArray;
 }
-//get_data("SELECT id, username, email FROM users WHERE id = ?");
+
+function get_data($command, $param, $values){
+    global $database;
+    $pre = $database->prepare($command);
+    $pre->bind_param($param, ...$values);
+    $pre->execute();
+    $result = $pre->get_result();
+    $pre->close();
+
+    return extract_rows($result);
+}
+// $data = get_data("SELECT nom, role FROM utilisateurs WHERE email = ?;", "s", ["admin@gmail.com"]);
+// print_r($data);
+
+$database->close();
 ?>
