f73429a59d2b03c68a256c0f438cff09730ddc22
Achraf-podcast <achrafagourram6@gmail.com>
2025-12-21 16:06:29 +0100

    Adjusting links in header

--- pages/header.php
+++ pages/header.php
@@ -20,7 +20,7 @@
         </head>
         ";
     }
-    function navbar(){
+    function navbar($links){
         echo "
         <header class='bg-white shadow-md sticky top-0 z-50'>
         <nav class='container mx-auto px-6 py-3 flex justify-between items-center'>
@@ -28,21 +28,21 @@
                 🦁 ASSAD - Zoo Virtuel
             </div>
             <div class='space-x-4 hidden md:flex'>
-                <a href='index.php' class='text-orange-600 font-semibold hover:text-orange-700 transition duration-300'>Accueil</a>
-                <a href='pages/animals.html' class='text-gray-600 hover:text-orange-700 transition duration-300'>Animaux</a>
+                <a href='{$links[0]}' class='text-orange-600 font-semibold hover:text-orange-700 transition duration-300'>Accueil</a>
+                <a href='{$links[1]}' class='text-gray-600 hover:text-orange-700 transition duration-300'>Animaux</a>
                 
         ";
 
         if(isset($_SESSION['loggedAccount'])){
             echo "
-                <a href='pages/visits.html' class='text-gray-600 hover:text-orange-700 transition duration-300'>Visites</a>
+                <a href='{$links[2]}' class='text-gray-600 hover:text-orange-700 transition duration-300'>Visites</a>
             </div>
             <div class='flex items-center space-x-2'>
             ";
             $connectedUser = extract_rows(request("SELECT * FROM utilisateurs WHERE id = ?;", "i", [$_SESSION['loggedAccount']]));
             if($connectedUser[0]['role'] == 'admin'){
                 echo "
-                    <a href='admin/users.php' name='admin' class='bg-transparent border border-orange-700 text-orange-700 px-4 py-2 rounded-lg hover:bg-orange-500 hover:text-white transition duration-300'>
+                    <a href='{$links[3]}' name='admin' class='bg-transparent border border-orange-700 text-orange-700 px-4 py-2 rounded-lg hover:bg-orange-500 hover:text-white transition duration-300'>
                         Admin
                     </a>
                 ";
